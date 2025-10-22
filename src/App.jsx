acf32739bef4c47025db1ea1f307cdd52142e89e
iksan <mdiksann@gmail.com>
2025-10-22 16:41:11 +0700

    Update personal information and project details in portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Iksan",
+  title: "Backend Developer",
+  email: "Iksan@example.com",
+  gitHub: "mdiksann",
+  instagram: "m_iksnnn",
+  linkedIn: "mdiksann",
   medium: "",
-  twitter: "microsoft",
+  twitter: "mdiksann",
   youTube: "Code",
 };
 
